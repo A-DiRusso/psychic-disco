d146376eea36df059aa0356c06fb46744d1a3141
Anthony DiRusso <anthonydirusso@Anthonys-MBP.T-mobile.com>
2019-02-19 15:15:01 -0500

    Makes more vowels

---
+++ extra_vowels.py
@@ -0,0 +1,12 @@
+short_a = 'happy'
+short_b = 'goal'
+short_c = 'hey'
+
+long_a = short_a[:2] + 'aaaaaa' + short_a[2:]
+long_b = short_b[:2] + 'ooooooooooooooooooooaaaaaaaaaaaaaaaaaaaaaa' + short_b[2:]
+long_c = short_c[:2] + 'eeeeeeeeeeeeeeeeeeeeeeeeeee' + short_c[2:]
+
+print(long_a)
+print(long_b)
+print(long_c)
+
