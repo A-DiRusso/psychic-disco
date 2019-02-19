d9eddcc38f16f316840e780653d7767b45620f1c
Anthony DiRusso <anthonydirusso@Anthonys-MBP.T-mobile.com>
2019-02-19 15:21:59 -0500

    finds smallest number

---
+++ smallest_num.py
@@ -0,0 +1,16 @@
+#starting with an empty list
+sum_numb = []
+
+# adding some random numbers
+sum_numb.append(43)
+sum_numb.append(4)
+sum_numb.append(3)
+sum_numb.append(143)
+sum_numb.append(413)
+sum_numb.append(431)
+sum_numb.append(1)
+
+# asking python what the smallest number in `sum_numb` is
+smallest_num = (min(sum_numb))
+# printing the answer python has for us
+print(smallest_num)
