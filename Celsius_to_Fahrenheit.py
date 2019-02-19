c52ea296181fa4858ef72da0a9111113bd8b46f5
Anthony DiRusso <anthonydirusso@Anthonys-MBP.T-mobile.com>
2019-02-19 15:03:26 -0500

    Made a temp converter

---
+++ Celsius_to_Fahrenheit.py
@@ -0,0 +1,5 @@
+#prompting the user for the temp in Celsius
+f_degree = int(input("What is the current temp in Celsius? "))
+
+#returning them the temp(roughly:) in fahrenheit.py
+print((f_degree * 1.8) + 32)
